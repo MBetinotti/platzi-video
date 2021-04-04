03a9e25dc032739b60e9b662cf44a2d3e69e0d94
MBetinotti <mbetinotti@gmail.com>
2021-04-04 17:54:08 -0300

    final curso de react

--- platzi-video/src/containers/App.jsx
+++ platzi-video/src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import Header from "../components/Header.jsx";
 import Search from "../components/Search";
@@ -6,33 +6,33 @@ import Categories from "../components/Categories";
 import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
 import Footer from "../components/Footer";
+import useInitialState from "../Hooks/useInitialState"
 
 import "../assets/styles/App.scss";
 
-export default function App() {
-  const [videos, setVideos] = useState([]);
+const API= "http://localhost:3000/initalState"
 
-  useEffect(() => {
-    fetch("http://localhost:3000/initalState")
-      .then((response) => response.json())
-      .then((data) => setVideos(data));
-  }, []);
+export default function App() {
+
+    const initialState = useInitialState(API)
 
   return (
     <div>
       <Header />
       <Search />
-      {videos.mylist.length > 0 && (
+      {initialState.mylist.length > 0 && (
         <Categories title="Mi Lista">
           <Carousel>
-            <CarouselItem />
+          {initialState.mylist.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
           </Carousel>
         </Categories>
       )}
 
       <Categories title="Tendencias">
         <Carousel>
-          {videos.trends.map((item) => (
+          {initialState.trends.map((item) => (
             <CarouselItem key={item.id} {...item} />
           ))}
         </Carousel>
@@ -40,7 +40,9 @@ export default function App() {
 
       <Categories title="Originales de Platzi Video">
         <Carousel>
-          <CarouselItem />
+        {initialState.originals.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
         </Carousel>
       </Categories>
       <Footer />
